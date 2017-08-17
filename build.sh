927a7726fbc910921e85523f18e7974e9194dfeb
Chris Peters <chris@minimalorange.com>
2017-08-17 12:10:38 -0400

    v0.2.0

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-BT_VERSION="0.1.1"
+BT_VERSION="0.2.0"
 BT_ZIP="BackgroundTasks-$BT_VERSION.zip"
 zip $BT_ZIP BackgroundTasks.cfc Task.cfc LICENSE README.md controllers/* db/*/* models/*
